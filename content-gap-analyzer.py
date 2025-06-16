b9849a86c9f7df9b8260acdfe04c05edc703b0ba
Ghettosaint <58518629+Ghettosaint@users.noreply.github.com>
2025-06-16 17:50:05 +0300

    Update content-gap-analyzer.py
    adding buy me a coffee

--- content-gap-analyzer.py
+++ content-gap-analyzer.py
@@ -1578,6 +1578,11 @@ def main():
     }
     </style>
     """, unsafe_allow_html=True)
+
+    # Buy Me a Coffee Widget
+    st.markdown("""
+    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="deyangeorgiev" data-description="Support me on Buy me a coffee!" data-message="If this tool has helped you, consider getting me a coffee :) Thanks!" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+    """, unsafe_allow_html=True)
     
     # Sidebar
     with st.sidebar:
